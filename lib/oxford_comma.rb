4e23cab125c88e66ea858ad094b34071bc656f2c
Matthew Jones <matts3371@gmail.com>
2019-11-12 01:08:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,10 @@
 def oxford_comma(array)
-  if array.size > 2 
-    array.insert(array.size - 1, "and")
-  end
 
   if array.size == 2 
     my_string = array.join(" and ")
   elsif array.size >= 3
     my_string = array.join(", ")
+    my_string.split(' ').insert(-2, 'and').join(' ')
     
   else
     my_string = array.join
