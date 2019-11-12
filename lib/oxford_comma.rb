5d9c1a6102b7a4060f7cbc89f1f7e148958976c7
Matthew Jones <matts3371@gmail.com>
2019-11-12 01:05:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,12 @@
 def oxford_comma(array)
   if array.size > 2 
     array.insert(array.size - 1, "and")
-    lastItem = array[array.size]
-    array.pop()
   end
 
   if array.size == 2 
     my_string = array.join(" and ")
   elsif array.size >= 3
     my_string = array.join(", ")
-    my_string += lastItem
     
   else
     my_string = array.join
