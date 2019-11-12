bad5921369eaa1ce63f97a7c1cf54acdc22afc3b
Matthew Jones <matts3371@gmail.com>
2019-11-12 01:10:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     my_string = array.join(" and ")
   elsif array.size >= 3
     my_string = array.join(", ")
-    my_string.split(' ').insert(-2, 'and').join(' ')
+    my_string.split(' ').insert(-3, 'and').join(' ')
     
   else
     my_string = array.join
