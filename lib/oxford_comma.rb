56b650880a322481d5b808dff65017a80b039daf
Matthew Jones <matts3371@gmail.com>
2019-11-12 01:03:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,8 @@
 def oxford_comma(array)
   if array.size > 2 
     array.insert(array.size - 1, "and")
-    lastItem = array.
+    lastItem = array[array.size]
+    array.pop()
   end
 
   if array.size == 2 
