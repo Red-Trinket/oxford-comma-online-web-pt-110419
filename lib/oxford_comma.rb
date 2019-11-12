622e365f9e6e6b6eadda78da4ee62a3fffd1b799
Matthew Jones <matts3371@gmail.com>
2019-11-12 00:52:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,11 @@
 def oxford_comma(array)
-  if array.size == 1 
-    array.join
-  end
-  elsif array.size == 2 
+  if array.size == 2 
     array.join(" and ")
   end
   elsif array.size >= 3
     array.join(", ")
   end 
   else
+    array.join
   end
 end
