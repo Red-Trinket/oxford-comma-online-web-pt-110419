0198e1d7b1ddb034ecc5c51d5e6d54acc816e2f6
Matthew Jones <matts3371@gmail.com>
2019-11-12 01:14:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
   if array.size > 3
-    last_item = array.pop()
+    last_item = array[array.size]
+    array.pop()
   if array.size == 2 
     my_string = array.join(" and ")
   elsif array.size >= 3
